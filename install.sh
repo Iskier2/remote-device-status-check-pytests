310b6192f2c9496a7483d081c9c93ad2b62493d4
Iskier2 <iskrafilip12@interia.pl>
2025-04-13 14:20:12 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 set -e  
 
-echo "🔧 Creating a virtual environment..."
+echo "Creating a virtual environment..."
 python3 -m venv venv
 
-echo "⚙️  Activating the virtual environment..."
+echo "Activating the virtual environment..."
 source venv/bin/activate
 
 echo "📦 Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "✅ Installation completed successfully!"
+echo "Installation completed successfully!"
